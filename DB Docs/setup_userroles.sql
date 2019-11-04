8487354794487501032b38311805042a3430e541
ravin.m88@gmail.com <ravin.m88@gmail.com>
2019-11-04 08:36:43 +0530

    user roles included modifiedtimestamp col

--- DB Docs/setup_userroles.sql
+++ DB Docs/setup_userroles.sql
@@ -1,10 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `setup` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `setup`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: setup
+-- Host: localhost    Database: setup
 -- ------------------------------------------------------
--- Server version	5.7.20-log
+-- Server version	8.0.17
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -29,6 +27,7 @@ CREATE TABLE `userroles` (
   `roletype` varchar(20) NOT NULL,
   `privileges` int(11) NOT NULL,
   `createdtimestamp` bigint(20) unsigned DEFAULT NULL,
+  `modifiedtimestamp` bigint(20) unsigned DEFAULT NULL,
   `hashkey` varchar(50) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `roletype_UNIQUE` (`roletype`)
@@ -41,7 +40,7 @@ CREATE TABLE `userroles` (
 
 LOCK TABLES `userroles` WRITE;
 /*!40000 ALTER TABLE `userroles` DISABLE KEYS */;
-INSERT INTO `userroles` VALUES (1,'ADMIN_PLUS',32767,1570872685185,'hashkey'),(2,'ADMIN',32767,1570872685185,'hashkey');
+INSERT INTO `userroles` VALUES (1,'ADMIN_PLUS',5,1570872685185,NULL,'hashkey'),(2,'ADMIN',3,1570872685185,NULL,'hashkey');
 /*!40000 ALTER TABLE `userroles` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -54,4 +53,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-10-13 10:48:37
+-- Dump completed on 2019-11-04  8:35:27
